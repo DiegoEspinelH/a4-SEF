1ee9160320d2d81fe573219c0389e5713032703c
Diego Espinel Hernandez <s4103630@student.rmit.edu.au>
2025-05-23 19:59:25 +1000

    fixed test file name error, starts with capital T, and wrote 3 tests to verify adding a person, more tests to be added

--- src/test/java/testPersonClass.java
+++ src/test/java/testPersonClass.java
@@ -2,13 +2,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class testPersonClass {
+class TestPersonClass {
     @Test
     public void testAddPerson_testcase1(){
         //test for correct input
-        Person testPerson = new Person("73@#!#abcPA", "james", "24|melbourne street|melbourne|Victoria|Australia", "25-05-2001");
-        assertFalse(testPerson.addPerson());
-    }
+        Person testPerson1 = new Person("73@#!#abPA", "james", "24|melbourne street|melbourne|Victoria|Australia", "25-05-2001");
+        assertTrue(testPerson1.addPerson());
+
+        //test with bad person ID - not correct length - shorter than 10 chars - len = 9
+        Person testPerson2 = new Person("73@#!#bPA", "james", "24|melbourne street|melbourne|Victoria|Australia", "25-05-2001");
+        assertFalse(testPerson2.addPerson());
+        
+        //test with bad person ID - not correct length - longer than 10 chars - len = 11
+        Person testPerson3 = new Person("73@#!#abcPA", "james", "24|melbourne street|melbourne|Victoria|Australia", "25-05-2001");
+        assertFalse(testPerson3.addPerson());
+
+
+    }   
     
 }
 
